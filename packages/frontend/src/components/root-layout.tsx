500f57424e284de565e7e7efe747df989fc6bb54
aiiiden <hjjam100@gmail.com>
2025-07-17 18:24:49 +0900

    chore: adjust gap in CommonLayout for improved spacing

--- packages/frontend/src/components/root-layout.tsx
+++ packages/frontend/src/components/root-layout.tsx
@@ -6,7 +6,7 @@ export default function CommonLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="border-2 border-black bg-white w-full max-w-(--max-container-width) mx-auto min-h-screen box-border flex gap-0">
+    <div className="border-2 border-black bg-white w-full max-w-(--max-container-width) mx-auto min-h-screen box-border flex -gap-1">
       <div className="border-r-2 basis-(--min-menu-width) md:basis-(--max-menu-width) shrink-0">
         {menu}
       </div>
