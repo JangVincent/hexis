57e05a1abb554456c5f0260ce1437a8509f90125
aiiiden <hjjam100@gmail.com>
2025-07-26 04:10:53 +0900

    chore: remove RPC URL from client configuration for security reasons

--- contract/lib/client.ts
+++ contract/lib/client.ts
@@ -2,7 +2,7 @@ import { createPublicClient, createWalletClient, http } from "viem";
 import { sepolia } from "viem/chains";
 import { userAccount } from "./account";
 
-const rpcUrl = "https://eth-sepolia.g.alchemy.com/v2/LSwqyW1bpcoV4HPbxiZa7";
+const rpcUrl = undefined;
 
 export const publicClient = createPublicClient({
   chain: sepolia,
