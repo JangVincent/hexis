17445a82a413136e402f74812a1d7129b7041b3e
aiiiden <aiiiden.dev@gmail.com>
2025-08-07 01:16:09 +0900

    refactor(frontend): restructure menu component and introduce MENU_ITEMS configuration

--- packages/frontend/src/components/menu/index.tsx
+++ packages/frontend/src/components/menu/index.tsx
@@ -1,24 +1,25 @@
-import { IconHome, IconSearch, IconSignIn } from '../icon';
+import { useMemo } from 'react';
 
 import HealthIndicator from '@/features/health/components/health-indicator';
 import LogoSection from './logo-section';
-import { MenuItem } from './menu-item';
+import MenuItem from './menu-item';
+import { MENU_ITEMS } from './config';
 
 function Menu() {
+  const menuItems = useMemo(
+    () =>
+      MENU_ITEMS.map(item => (
+        <li key={item.to}>
+          <MenuItem to={item.to} icon={item.icon} label={item.label} />
+        </li>
+      )),
+    []
+  );
+
   return (
     <nav className='flex flex-col h-full'>
       <LogoSection />
-      <menu className='[&>li]:border-b-2'>
-        <li>
-          <MenuItem to='/' icon={IconHome} label='HOME' />
-        </li>
-        <li>
-          <MenuItem to='/search' icon={IconSearch} label='SEARCH' />
-        </li>
-        <li>
-          <MenuItem to='/account/sign-in' icon={IconSignIn} label='SIGN IN' />
-        </li>
-      </menu>
+      <menu className='[&>li]:border-b-2'>{menuItems}</menu>
       <div className='flex-grow flex flex-col justify-end'>
         <HealthIndicator />
       </div>
--- packages/frontend/src/components/menu/menu-item.tsx
+++ packages/frontend/src/components/menu/menu-item.tsx
@@ -1,21 +1,38 @@
-import { Link } from '@tanstack/react-router';
+import type { RoutePath } from '@/lib/route-type';
+import { cn } from '@/lib/tailwind-utils';
+import { Link, useMatchRoute } from '@tanstack/react-router';
 import React from 'react';
 
-export function MenuItem({
+export default function MenuItem({
   to,
   icon: Icon,
   label,
 }: {
-  to: string;
+  to: RoutePath;
   icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
   label: string;
 }) {
+  const matchRoute = useMatchRoute();
+
+  const active = matchRoute({
+    from: to,
+    to,
+  });
+
   return (
     <Link
       to={to}
-      className='flex items-center justify-center md:justify-start gap-3 p-4 hover:bg-black focus:bg-black group'
+      className={cn(
+        'flex items-center justify-center md:justify-start gap-3 p-4 hover:bg-black focus:bg-black group',
+        active && 'bg-black text-white'
+      )}
     >
-      <Icon className='size-6 md:size-8 group-hover:text-white group-focus:text-white' />
+      <Icon
+        className={cn(
+          'size-6 md:size-8 group-hover:text-white group-focus:text-white',
+          active && 'text-white'
+        )}
+      />
       <span className='hidden md:block text-xl font-bold group-hover:text-white group-focus:text-white'>
         {label}
       </span>
