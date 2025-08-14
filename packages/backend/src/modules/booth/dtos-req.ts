95578291931c823f86e424574b8166fb93cbf9d2
JangVincent <vincentjangdev@gmail.com>
2025-08-14 20:21:12 +0900

    add : booth API

---
+++ packages/backend/src/modules/booth/dtos-req.ts
@@ -0,0 +1,36 @@
+import { EthereumAddressValidator } from '@commons/vaildators/ethereumAddress.validator';
+import z from 'zod';
+
+export const GetBoothPaginationDtoValidationScheme = z.object({
+  page: z.coerce
+    .number()
+    .min(1)
+    .default(1)
+    .transform(val => Number(val)),
+  size: z.coerce
+    .number()
+    .min(1)
+    .default(10)
+    .transform(val => Number(val)),
+});
+
+export type GetBoothPaginationDTO = z.infer<
+  typeof GetBoothPaginationDtoValidationScheme
+>;
+
+export const GetBoothDtoValidationScheme = z.object({
+  boothId: z.string(),
+});
+
+export type GetBoothDTO = z.infer<typeof GetBoothDtoValidationScheme>;
+
+export const CreateBoothDtoValidationScheme = z.object({
+  owner: EthereumAddressValidator,
+  sampleText: z.string(),
+  blockNumber: z.string().min(1).regex(/^\d+$/, {
+    message: 'blockNumber should be a string of numbers',
+  }),
+  fullText: z.string(),
+});
+
+export type CreateBoothDTO = z.infer<typeof CreateBoothDtoValidationScheme>;
