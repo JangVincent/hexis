2141a3261128f8edb2b1e164169565eee0d4d8be
JangVincent <vincentjangdev@gmail.com>
2025-08-14 20:20:58 +0900

    add : booth relative schema

--- packages/backend/db/schema.ts
+++ packages/backend/db/schema.ts
@@ -1,4 +1,11 @@
-import { pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from 'drizzle-orm/pg-core';
 
 export const usersTable = pgTable('users', {
   id: uuid('id').default(crypto.randomUUID()).primaryKey().notNull(),
@@ -10,8 +17,38 @@ export type NewUser = typeof usersTable.$inferInsert;
 
 export const noncesTable = pgTable('nonces', {
   nonce: varchar({ length: 255 }).primaryKey().notNull(),
-  walletAddress: varchar({ length: 255 }).notNull(),
+  walletAddress: varchar({ length: 42 }).notNull(),
   expiresAt: timestamp('expires_at').notNull(),
 });
 export type Nonce = typeof noncesTable.$inferSelect;
 export type NewNonce = typeof noncesTable.$inferInsert;
+
+// Booth
+
+export const rotateKeyTable = pgTable('rotate_key', {
+  id: serial('id').primaryKey().notNull(),
+  key: varchar({ length: 256 }).notNull(),
+});
+export type RotateKey = typeof rotateKeyTable.$inferSelect;
+export type NewRotateKey = typeof rotateKeyTable.$inferInsert;
+
+export const boothTable = pgTable('booths', {
+  id: uuid('id').default(crypto.randomUUID()).primaryKey().notNull(),
+  owner: varchar({ length: 42 }).notNull(), //wallet address
+  sampleText: text('sample_text').notNull(),
+  blockNumber: varchar({ length: 100 }).notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+});
+export type Booth = typeof boothTable.$inferSelect;
+export type NewBooth = typeof boothTable.$inferInsert;
+
+export const boothSaleTextTable = pgTable('booth_sale_text', {
+  id: uuid('id').default(crypto.randomUUID()).primaryKey().notNull(),
+  boothId: uuid('booth_id')
+    .notNull()
+    .references(() => boothTable.id),
+  text: text('text').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+});
+export type BoothSaleText = typeof boothSaleTextTable.$inferSelect;
+export type NewBoothSaleText = typeof boothSaleTextTable.$inferInsert;
